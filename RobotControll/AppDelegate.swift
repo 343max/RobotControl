440c1a5436db6098cc6fcf800d3feafb5fa88d3e
Max von Webel <max@343max.de>
2018-04-20 22:26:59 -0700

    Initial Commit

---
+++ RobotControll/AppDelegate.swift
@@ -0,0 +1,21 @@
+// Copyright 2013-present Facebook. All Rights Reserved.
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
